c9211ba75caed9117eac5ef4958ea52e8e523145
OKI Ayobami <46370698+xlassix@users.noreply.github.com>
2020-06-16 19:31:07 +0100

    Delete actions.ts

--- src/store/history/actions.ts
+++
@@ -1,8 +0,0 @@
-import {HistoryState, UPDATE_HISTORY, HistoryActionTypes } from './types'
-
-export function updateHistory(newObject: HistoryState): HistoryActionTypes {
-  return {
-    type: UPDATE_HISTORY,
-    payload: newObject
-  }
-}
